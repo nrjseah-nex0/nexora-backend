ac9d6891ccea817ca7afc0be8ae9a1bba62068cb
nrjseah-nex0 <nrjseah00@gmail.com>
2026-03-16 10:17:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("🚀 NEXORA Backend is Running");
-});
+const campaigns=require("./routes/campaigns")
+const analytics=require("./routes/analytics")
+const products=require("./routes/products")
+const webhook=require("./routes/webhook")
 
-app.get("/products", (req, res) => {
-  res.json([
-    {name:"عطر فاخر", sales:120, price:250},
-    {name:"ساعة رجالية", sales:80, price:300},
-    {name:"نظارة شمسية", sales:65, price:150}
-  ]);
-});
+app.use(campaigns)
+app.use(analytics)
+app.use(products)
+app.use(webhook)
 
-app.get("/analytics", (req, res) => {
-  res.json({
-    monthlyRevenue: 12500,
-    completedOrders: 210,
-    topProduct: "عطر فاخر"
-  });
-});
-
-app.listen(3000, () => {
-  console.log("NEXORA backend running on port 3000");
-});
+app.listen(3000,()=>{
+ console.log("🚀 NEXORA Backend Running")
+})
